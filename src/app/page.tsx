82c06d0ae59d56041717d339f2be78974ca2968b
junaid javed <junaid61155@gmail.com>
2025-02-22 00:32:43 +0800

    Added the particle effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import { LandingPage } from "./myComponents/LandingPage";
+import { Particles } from "@/components/magicui/particles";
 
 export default function Home() {
   return (
-    <>
+    <div className="relative h-screen w-full">
+      <Particles
+        quantity={300}
+        ease={100}
+        color="black"
+        className="absolute inset-0 "
+      />
+
       <LandingPage />
-    </>
+    </div>
   );
 }
